663707f851b756ec332b8e890341eba0f1f5d3cf
manighahrmani <mashapicasso@gmail.com>
2023-12-25 05:04:56 +0000

    Refactor calculateArea and add celebrate function

--- pract16.dart
+++ pract16.dart
@@ -1,3 +1,21 @@
 void main() {
-  print('Hello, World!');
+  print(calculateArea(side1: 5, side2: 10));
+  print(calculateArea(side1: 5));
+  celebrate(10, name: "Alice");
+}
+
+// int calculateArea(int side1, int side2) {
+//   return side1 * side2;
+// }
+
+// int calculateArea({required int side1, int side2 = 1}) {
+//   return side1 * side2;
+// }
+
+int calculateArea({required int side1, int? side2}) {
+  return side1 * (side2 ?? 1);
+}
+
+void celebrate(int age, {String name = "friend"}) {
+  print("Happy $age" + "th birthday, $name!");
 }
