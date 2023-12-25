62202caa43bc9b095b29f442a930a06bba7a20e7
manighahrmani <mashapicasso@gmail.com>
2023-12-25 05:56:30 +0000

    Refactor examReminder function

--- lect16.dart
+++ lect16.dart
@@ -1,5 +1,7 @@
 void main() {
+  // examReminder("Alice", 14);
   examReminder(name: "Alice", date: 14);
+  examReminder(date: 14, name: "Alice");
   examReminder(date: 14);
 }
 
@@ -8,15 +10,15 @@ void main() {
 //       "Don't forget your exam on the ${date}th!");
 // }
 
-// void examReminder({String name = "student", required int date}) {
-//   print("Dear $name,\n"
-//       "Don't forget your exam on the ${date}th!");
-// }
-
-void examReminder({
-  String? name, 
-  required int date
-}) {  
-  print("Dear ${name ?? 'student'},\n"
+void examReminder({String name = "student", required int date}) {
+  print("Dear $name,\n"
       "Don't forget your exam on the ${date}th!");
 }
+
+// void examReminder({
+//   String? name,
+//   required int date
+// }) {
+//   print("Dear ${name ?? 'student'},\n"
+//       "Don't forget your exam on the ${date}th!");
+// }
