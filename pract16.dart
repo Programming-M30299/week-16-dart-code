ab899b1c22e0ca8a9d06c0b7b120a7b8dcf8a2b0
manighahrmani <mashapicasso@gmail.com>
2023-12-25 19:39:03 +0000

    Update import statement in pract16.dart

--- pract16.dart
+++ pract16.dart
@@ -15,7 +15,7 @@ int calculateArea({required int side1, int? side2}) {
   return side1 * (side2 ?? 1);
 }
 
-// This function is imported in `birthday.dart`.
+// This function is imported in `main.dart`.
 void birthdayMessage(String name, int age) {
   print("Happy birthday, $name! You are $age years old!");
 }
