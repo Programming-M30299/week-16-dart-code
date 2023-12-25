99988e99bdc7de33ad2cb65a4c9b64d3b3778221
manighahrmani <mashapicasso@gmail.com>
2023-12-25 03:52:40 +0000

    Add exam Reminder to lecture file

--- lect16.dart
+++ lect16.dart
@@ -1,3 +1,22 @@
 void main() {
-  print('Hello, World!');
+  examReminder(name: "Alice", date: 14);
+  examReminder(date: 14);
+}
+
+// void examReminder(String name, int date) {
+//   print("Dear $name,\n"
+//       "Don't forget your exam on the ${date}th!");
+// }
+
+// void examReminder({String name = "student", required int date}) {
+//   print("Dear $name,\n"
+//       "Don't forget your exam on the ${date}th!");
+// }
+
+void examReminder({
+  String? name, 
+  required int date
+}) {  
+  print("Dear ${name ?? 'student'},\n"
+      "Don't forget your exam on the ${date}th!");
 }
