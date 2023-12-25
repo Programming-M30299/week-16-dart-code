de8239135c62237b7542f4c5fbd0bb2169bab2f2
manighahrmani <mashapicasso@gmail.com>
2023-12-25 05:05:06 +0000

    Remove unused celebrate function

--- pract16.dart
+++ pract16.dart
@@ -1,7 +1,6 @@
 void main() {
   print(calculateArea(side1: 5, side2: 10));
   print(calculateArea(side1: 5));
-  celebrate(10, name: "Alice");
 }
 
 // int calculateArea(int side1, int side2) {
@@ -14,8 +13,4 @@ void main() {
 
 int calculateArea({required int side1, int? side2}) {
   return side1 * (side2 ?? 1);
-}
-
-void celebrate(int age, {String name = "friend"}) {
-  print("Happy $age" + "th birthday, $name!");
 }
