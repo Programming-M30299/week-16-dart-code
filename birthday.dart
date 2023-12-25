1fe47455cbfb582e76c817ac614809a8c3bcec6c
manighahrmani <mashapicasso@gmail.com>
2023-12-25 19:35:21 +0000

    import birthday message from pract 16

---
+++ birthday.dart
@@ -0,0 +1,16 @@
+import 'pract16.dart';
+
+/* 
+This program prints a birthday message for a given user.
+It takes two command line arguments: the user's name and age.
+
+Run this program with the following command:
+```
+dart birthday.dart "John Doe" 21
+```
+ */
+void main(List<String> arguments) {
+  String name = arguments[0];
+  int age = int.parse(arguments[1]);
+  birthdayMessage(name, age);
+}
