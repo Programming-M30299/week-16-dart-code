3aa6a5943129d59464353c648436a704e01143ee
manighahrmani <mashapicasso@gmail.com>
2023-12-26 04:39:10 +0000

    Add import and new function for calculating circle area

--- pract16.dart
+++ pract16.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   // print(calculateArea(side1: 5, side2: 10));
   // print(calculateArea(side1: 5));
@@ -18,11 +20,17 @@ void calculateAge(String name, int birthYear) {
 //   return side1 * side2;
 // }
 
-int calculateArea({required int side1, int? side2}) {
-  return side1 * (side2 ?? 1);
-}
+// int calculateArea({required int side1, int? side2}) {
+//   return side1 * (side2 ?? 1);
+// }
+
+int calculateArea(int side1, {int side2 = 1}) => side1 * side2;
 
 // This function is imported in `main.dart`.
 void birthdayMessage(String name, int age) {
   print("Happy birthday, $name! You are $age years old!");
+}
+
+double calculateCircleArea(double radius) {
+  return pi * pow(radius, 2);
 }
