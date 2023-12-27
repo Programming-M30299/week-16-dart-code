089d35742d5d80205ea49f53c42a036f0cedeb19
manighahrmani <mashapicasso@gmail.com>
2023-12-27 00:27:52 +0000

    Refactor code: Uncomment examReminder calls and update formatAndPrint function signature

--- lect16.dart
+++ lect16.dart
@@ -1,8 +1,11 @@
 void main() {
   // examReminder("Alice", 14);
-  examReminder(name: "Alice", date: 14);
-  examReminder(date: 14, name: "Alice");
-  examReminder(date: 14);
+  // examReminder(name: "Alice", date: 14);
+  // examReminder(date: 14, name: "Alice");
+  // examReminder(date: 14);
+
+  // formatAndPrint(timesTwo, "Hello");
+  formatAndPrint(loudify, "Hello");
 }
 
 // void examReminder(String name, int date) {
@@ -22,3 +25,23 @@ void examReminder({String name = "student", required int date}) {
 //   print("Dear ${name ?? 'student'},\n"
 //       "Don't forget your exam on the ${date}th!");
 // }
+
+int timesTwo(int x) => x * 2;
+
+String loudify(String text) => '${text.toUpperCase()}!!!';
+
+// void formatAndPrint(
+//   String Function(String) formatFunction, 
+//   String text
+// ) {
+//   String formattedText = formatFunction(text);
+//   print(formattedText);
+// }
+
+void formatAndPrint(
+  Function formatFunction, 
+  String text
+) {
+  String formattedText = formatFunction(text);
+  print(formattedText);
+}
