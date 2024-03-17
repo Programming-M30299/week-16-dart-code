1f9da6fb9ae5c2039dbfc9a8f7636f4f90f4dd3e
manighahrmani <mashapicasso@gmail.com>
2024-03-17 14:54:37 +0000

    Remove higher order and breaks

--- pract16.dart
+++ pract16.dart
@@ -2,16 +2,10 @@ import 'dart:math';
 import 'dart:io';
 
 void main() {
-  print("Uncomment the code in `main()` to run the examples.");
+  print("Uncomment the code in this file to run the examples.");
 
   // print(calculateArea(side1: 5, side2: 10));
   // print(calculateArea(side1: 5));
-
-  // int Function(int) multiplyBy2 = (int x) => x * 2;
-  // int Function(int) multiplyBy2 = makeMultiplier(2);
-  // int number = 5;
-  // int result = applyTwice(multiplyBy2, number);
-  // print(result); // 20
 
   // divide(5, 0);
   // divide(5, 2);
@@ -50,51 +44,46 @@ double calculateCircleArea(double radius) {
   return pi * pow(radius, 2);
 }
 
+int multiplyBy2(int x) {
+  return x * 2;
+}
+
+// The same function, but using the arrow syntax
 // int multiplyBy2(int x) => x * 2;
 
-int applyTwice(int Function(int) operation, int number) {
-  int firstStep = operation(number);
-  int secondStep = operation(firstStep);
-  return secondStep;
-}
-
-int Function(int) makeMultiplier(int x) {
-  return (int y) => x * y;
-}
-
 void multiLingualGreeting(String language) {
-  // if (language == 'en') {
-  //   print('Hello, world!');
-  //   print('You selected English.');
-  // } else if (language == 'es') {
-  //   print('Hola, mundo!');
-  //   print('Has seleccionado Español.');
-  // } else if (language == 'fr') {
-  //   print('Bonjour, monde!');
-  //   print('Vous avez sélectionné le Français.');
-  // } else {
-  //   print('Unknown language');
-  //   print('Please select a valid language.');
-  // }
-
-  switch (language) {
-    case 'en-gb' || 'en-us':
-      print('Hello, world!');
-      print('You selected English.');
-      break;
-    case 'es':
-      print('Hola, mundo!');
-      print('Has seleccionado Español.');
-      break;
-    case 'fr':
-      print('Bonjour, monde!');
-      print('Vous avez sélectionné le Français.');
-      break;
-    default:
-      print('Unknown language');
-      print('Please select a valid language.');
+  if (language == 'en') {
+    print('Hello, world!');
+    print('You selected English.');
+  } else if (language == 'es') {
+    print('Hola, mundo!');
+    print('Has seleccionado Español.');
+  } else if (language == 'fr') {
+    print('Bonjour, monde!');
+    print('Vous avez sélectionné le Français.');
+  } else {
+    print('Unknown language');
+    print('Please select a valid language.');
   }
 }
+
+// The same function, but using the switch statement
+// void multiLingualGreeting(String language) {
+//   switch (language) {
+//     case 'en-gb' || 'en-us':
+//       print('Hello, world!');
+//       print('You selected English.');
+//     case 'es':
+//       print('Hola, mundo!');
+//       print('Has seleccionado Español.');
+//     case 'fr':
+//       print('Bonjour, monde!');
+//       print('Vous avez sélectionné le Français.');
+//     default:
+//       print('Unknown language');
+//       print('Please select a valid language.');
+//   }
+// }
 
 double customLog(double x, {double? base}) {
   switch (base) {
@@ -112,10 +101,8 @@ void getBoolFromUser() {
   switch (isTrue) {
     case true:
       print('It is true');
-      break;
     case false:
       print('It is false');
-      break;
     // Without the default case, the analyzer would complain
     // that the switch statement doesn't cover all cases
     default:
