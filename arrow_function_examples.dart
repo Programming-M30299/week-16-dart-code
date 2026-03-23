eea802c314c1d85ee07f8681608e159be742e04b
manighahrmani <mashapicasso@gmail.com>
2026-03-23 16:56:32 +0000

    Add examples of arrow functions

---
+++ arrow_function_examples.dart
@@ -0,0 +1,17 @@
+// void greet(String name){
+//   print("Hello, $name!");
+// }
+
+void greet(String name) => print("Hello, $name!");
+
+// int add(int a, int b){
+//   return a + b;
+// }
+
+int add(int a, int b) => a + b;
+
+// String loudify(String message) {
+//   return message.toUpperCase() + "!!!";
+// }
+
+String loudify(String message) => message.toUpperCase() + "!!!";
